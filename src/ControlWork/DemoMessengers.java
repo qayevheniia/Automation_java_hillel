7d6e239d3c22b5496587e6d0a9f096239ad18380
Yevgeniia Motytskaya <qayevheniia@gmail.com>
2021-07-26 20:27:52 +0300

    Create Messenger

---
+++ src/ControlWork/DemoMessengers.java
@@ -0,0 +1,16 @@
+package ControlWork;
+
+import Lessons_10.Task.Shape;
+
+public class DemoMessengers {
+
+    public static void main(String[] args) {
+        Users[] users = new Users[2];
+        users[0] = new Users("Motytskaya Eugeniia", 1, "12345", "Zhenya");
+        users[1] = new Users("Motytskai Igor", 2, "12345", "Igor");
+
+
+
+
+    }
+}
---
+++ src/ControlWork/Messages.java
@@ -0,0 +1,15 @@
+package ControlWork;
+
+public class Messages {
+    int Message;
+
+    public void addNewMessage( int Message) {
+
+        System.out.println();
+    }
+    public void deleteMessage( int Message) {
+
+        System.out.println();
+    }
+
+}
